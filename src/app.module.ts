d0cdfd62a8e8bec0f5f29091a44660638e774582
Alberto Benatti <alberto-benatti@dedagroup.it>
2023-04-19 09:38:44 +0200

    init message app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { CreationModule } from './creation/creation.module';
 import { NestCourseModule } from './nest-course/nest-course.module';
+import { MessagesModule } from './nest-course/messages/messages.module';
+import { Folder2Controller } from './folder1/folder2.controller';
 
 @Module({
-  imports: [CoursesModule, CreationModule, NestCourseModule],
-  controllers: [AppController],
+  imports: [CoursesModule, CreationModule, NestCourseModule, MessagesModule],
+  controllers: [AppController, Folder2Controller],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/folder1/folder2.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('folder2')
+export class Folder2Controller {}
