78dd3d6fade4971c681134dea81140f0329f8269
Alberto Benatti <alberto-benatti@dedagroup.it>
2023-04-19 09:59:00 +0200

    controller dec made

--- src/nest-course/messages/messages.controller.ts
+++ src/nest-course/messages/messages.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+  @Get('')
+  listMessages() {
+    this.messagesService.listMessages();
+  }
+  @Post('')
+  createMessage(@Body() body: any) {
+    this.messagesService.createMessage(body);
+  }
+
+  @Delete('')
+  deleteMessage() {
+    this.messagesService.deleteMessage();
+  }
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    this.messagesService.getMessage(id);
+  }
+}
--- src/nest-course/messages/messages.service.ts
+++ src/nest-course/messages/messages.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  listMessages() {
+    /* TODO document why this method 'listMessages' is empty */
+  }
+
+  deleteMessage() {
+    /* TODO document why this method 'deleteMessage' is empty */
+  }
+
+  getMessage(id) {
+    /* TODO document why this method 'getMessage' is empty */
+  }
+
+  createMessage(body: any) {
+    /* TODO document why this method 'createMessage' is empty */
+  }
+}
