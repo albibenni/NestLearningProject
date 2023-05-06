ab409f66f0b620c93f6f947eb8fec46b50e048bc
Alberto Benatti <albi.benni8@gmail.com>
2023-05-06 16:25:43 +0200

    big app module creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { CreationModule } from './creation/creation.module';
 import { NestCourseModule } from './nest-course/nest-course.module';
 import { MessagesModule } from './nest-course/messages/messages.module';
 import { Folder2Controller } from './folder1/folder2.controller';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './myCV/users/users.module';
+import { ReportsModule } from './myCV/reports/reports.module';
 
 @Module({
-  imports: [CoursesModule, CreationModule, NestCourseModule, MessagesModule],
+  imports: [
+    CoursesModule,
+    CreationModule,
+    NestCourseModule,
+    MessagesModule,
+    UserModule,
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController, Folder2Controller],
   providers: [AppService],
 })
