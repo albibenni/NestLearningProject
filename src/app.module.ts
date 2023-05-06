d5c09ca8e7711150c094bb4bcfe15beb2ed6e0de
Alberto Benatti <albi.benni8@gmail.com>
2023-05-06 17:34:54 +0200

    typeorm with sqlite init

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,22 @@ import { CreationModule } from './creation/creation.module';
 import { NestCourseModule } from './nest-course/nest-course.module';
 import { MessagesModule } from './nest-course/messages/messages.module';
 import { Folder2Controller } from './folder1/folder2.controller';
-import { UserModule } from './user/user.module';
 import { UsersModule } from './myCV/users/users.module';
 import { ReportsModule } from './myCV/reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [], // our entities
+      synchronize: true,
+    }),
     CoursesModule,
     CreationModule,
     NestCourseModule,
     MessagesModule,
-    UserModule,
     UsersModule,
     ReportsModule,
   ],
