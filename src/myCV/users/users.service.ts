a0b1f1fc2b39475c3fae872c3409cd659dd34284
Alberto Benatti <albi.benni8@gmail.com>
2023-05-07 20:56:27 +0200

    inject repo in service

--- src/myCV/users/users.service.ts
+++ src/myCV/users/users.service.ts
@@ -1,10 +1,17 @@
-import { Body, Injectable, Post } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
   async createUser(createUser: CreateUserDto) {
-    return Promise.resolve(createUser);
+    const email = createUser.email;
+    const password = createUser.password;
+    const user = this.repo.create({ email, password });
+    return Promise.resolve(user);
   }
 
   async deleteUser(userId: any) {
