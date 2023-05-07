0954247aa9afea1f3ef2b81e6e809ef17ffa910e
Alberto Benatti <albi.benni8@gmail.com>
2023-05-07 16:45:51 +0200

    service wip

--- src/myCV/users/users.service.ts
+++ src/myCV/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async createUser(createUser: CreateUserDto) {
+    return Promise.resolve(undefined);
+  }
+
+  async deleteUser(userId: any) {
+    return Promise.resolve(undefined);
+  }
+
+  async getUserById(userId: string) {
+    return Promise.resolve(undefined);
+  }
+
+  async getUsers() {
+    return Promise.resolve(undefined);
+  }
+}
