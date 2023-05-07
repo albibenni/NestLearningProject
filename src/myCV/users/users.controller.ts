f7def6e6b4b38324bfa1bc111e5c7aad9f18c7e7
Alberto Benatti <albi.benni8@gmail.com>
2023-05-07 16:42:57 +0200

    controller and dto created

--- src/myCV/users/users.controller.ts
+++ src/myCV/users/users.controller.ts
@@ -1,6 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
-  constructor(private readonly) {}
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  async getUsers() {
+    return await this.usersService.getUsers();
+  }
+  @Get(':courseId')
+  async getCourseById(@Param('userId') userId: string) {
+    return await this.usersService.getUserById(userId);
+  }
+
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return await this.usersService.createUser(createUser);
+  }
+  @Delete()
+  async deleteCourse(@Query() query: any) {
+    return await this.usersService.deleteUser(query.userId);
+  }
 }
